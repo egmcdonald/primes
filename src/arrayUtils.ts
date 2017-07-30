26cb82e257a5fb40dcb156ce4077b94a66b09838
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-31 00:00:20 +0100

    Added functionality to generateBooleanArray to ensure passing tests

--- src/arrayUtils.ts
+++ src/arrayUtils.ts
@@ -6,3 +6,12 @@ export const isNxN = (source: Array<Array<number>>): boolean => {
         if (source[i].length != n) return false;
     return true;
 }
+
+export const generateBooleanArray = (length: number, bit: boolean): Array<boolean> => {
+    if (length < 0) throw new RangeError('length cannot be less than 0');
+    
+    var array: Array<boolean> = Array<boolean>(length);
+    for (var i = 0; i < array.length; i++)
+        array[i] = bit;
+    return array;
+}
