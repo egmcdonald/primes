f0c66919936f28c90ea0b98464dfa01bc203fafd
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-30 21:23:20 +0100

    Updated primes route spec to expected outcomes

--- tests/routes/primes.spec.js
+++ tests/routes/primes.spec.js
@@ -15,7 +15,7 @@ describe ('routes/Primes', () => {
         it('should return multiplication table of primes up to param n', () => {
             var req = { params: { 'n': 3 } };
             var res = app.makeResponse((err, sideEffects) => {
-                var source = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+                var source = [[0, 1, 2, 3], [1, 1, 2, 3], [2, 2, 4, 6], [3, 3, 6, 9]];
                 assert.equal(sideEffects.send, FormatUtils.formatHashsetToTable(source));              
             });
             Primes.route(req, res);
@@ -25,7 +25,6 @@ describe ('routes/Primes', () => {
         it('should return bad request if n is invalid', () => {
             var req = { params: { 'n': 'abc' } };
             var res = app.makeResponse((err, sideEffects) => {
-                assert.equal(sideEffects.status, 400);
                 assert.equal(sideEffects.send, 'input [abc] type invalid: not a number');
             });
             Primes.route(req, res);
@@ -33,7 +32,6 @@ describe ('routes/Primes', () => {
         it('should return bad request if n is outwith range', () => {
             var req = { params: { 'n': 0 } };
             var res = app.makeResponse((err, sideEffects) => {
-                assert.equal(sideEffects.status, 400);
                 assert.equal(sideEffects.send, 'input must be within 1 to 2147483647 range');
             });
             Primes.route(req, res);
