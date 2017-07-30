b39b979ba1eff2e9b4e6b935b162ca85b3207a49
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-30 20:20:23 +0100

    Updated format utility to use new double array approach and simplified function to only perform formatting (instead of manipulation and formatting as previously)

--- src/formatUtils.ts
+++ src/formatUtils.ts
@@ -1,22 +1,14 @@
-//TODO: will likely change this function to take a set depending on performance
-export const formatMultiplicationTable = (source: Array<number>): string => {
+import * as ArrayUtils from './arrayUtils';
+
+export const formatHashsetToTable = (source: Array<Array<number>>): string => {
     if (source.length == 0) throw new RangeError('source array cannot be empty');
-        
-    var table: string = '|\t\t|';
+    if (ArrayUtils.isNxN(source) === false) throw new RangeError('source array is not n x n');       
 
+    var table: string = '';
     for(var i = 0; i < source.length; i++) {
-        table += '\t'.concat(source[i].toString(), '\t|');
-    }
-
-    table += '\r\n';
-
-    for(var j = 0; j < source.length; j++) {
-        table += '|\t'.concat(source[j].toString(), '\t|');
-        for(var k = 0; k < source.length; k++) {                    
-            var entry: string = (source[j] * source[k]).toString();                
-            table += '\t'.concat(entry, '\t|');
-        }
-        table += '\r\n';
+        table += '|\t';
+        table += source[i].join('\t|\t');    
+        table += '\t|\r\n';
     }
     return table;
 }
