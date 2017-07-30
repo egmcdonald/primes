d2ef510ed4807583a46e6890505922b5240cbbe3
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-30 20:18:52 +0100

    Updated format utility tests to take a double array of values, added spec for array utility that will check if a double array is n x n

---
+++ tests/arrayUtils.spec.js
@@ -0,0 +1,22 @@
+//chai
+var chai = require('chai');
+var assert = chai.assert;
+
+var ArrayUtils = require('../scripts/arrayUtils');
+
+describe('ArrayUtils', () => {
+    describe('isNxN', () => {
+        //success cases
+        it('should return true if double array is n x n', () => {
+            const source = [[1, 2, 3], [3, 4, 5], [6, 7, 8]];
+            assert.isTrue(ArrayUtils.isNxN(source));
+        });
+        it('should return false if double array is not n x n', () => {
+            const source = [[1, 2, 3], [3, 5], [6, 7, 8]];
+            assert.isFalse(ArrayUtils.isNxN(source));
+        });   
+        
+        //fail cases                    
+        it('should throw range error if double array is empty', () => assert.throws(() => ArrayUtils.isNxN([0][0]), RangeError, 'source array cannot be empty'));
+    });
+});
--- tests/formatUtils.spec.js
+++ tests/formatUtils.spec.js
@@ -6,23 +6,26 @@ var FormatUtils = require('../scripts/formatUtils');
 var NumberUtils = require('../scripts/numberUtils');
 
 describe('FormatUtils', () => {
-    describe('formatMultiplicationTable', () => {
+    describe('formatHashsetToTable', () => {
         //success cases
-        it('should not throw if source array is not empty', () => assert.doesNotThrow(() => FormatUtils.formatMultiplicationTable([1, 2, 3])));
         it('should format a source array into a multiplication table', () => {
             const source = NumberUtils.generateMultiplicationHashSet([1, 2, 3]);
             
-            var actual = FormatUtils.formatMultiplicationTable(source);
+            var actual = FormatUtils.formatHashsetToTable(source);
 
-            const expected = '|\t\t|\t1\t|\t2\t|\t3\t|\r\n' +
-                            '|\t1\t|\t1\t|\t2\t|\t3\t|\r\n' +
-                            '|\t2\t|\t2\t|\t4\t|\t6\t|\r\n' +
-                            '|\t3\t|\t3\t|\t6\t|\t9\t|\r\n';
+            const expected = '|\t0\t|\t1\t|\t2\t|\t3\t|\r\n' +
+                             '|\t1\t|\t1\t|\t2\t|\t3\t|\r\n' +
+                             '|\t2\t|\t2\t|\t4\t|\t6\t|\r\n' +
+                             '|\t3\t|\t3\t|\t6\t|\t9\t|\r\n';
 
             assert.equal(actual, expected);
         });
 
         //fail cases
-        it('should throw if source array is empty', () => assert.throws(() => FormatUtils.formatMultiplicationTable([]), RangeError, 'source array cannot be empty'));
+        it('should throw if source array is empty', () => assert.throws(() => FormatUtils.formatHashsetToTable([0][0]), RangeError, 'source array cannot be empty'));
+        it('should throw if source array is not n x n', () => {
+            const source = [[0, 1, 2], [1, 2], [2, 3, 4]];
+            assert.throws(() => FormatUtils.formatHashsetToTable(source), RangeError, 'source array is not n x n');                        
+        });
     });
 });
