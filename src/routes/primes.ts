43eac8c3b74c5e7a29ec0534b63817f06b7ec51d
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-30 21:24:01 +0100

    Updated primes route to pass fail case tests

--- src/routes/primes.ts
+++ src/routes/primes.ts
@@ -1,5 +1,22 @@
 import * as express from 'express';
 
+import * as NumberUtils from './../NumberUtils';
+import * as FormatUtils from './../FormatUtils';
+
 export const route = (req: express.Request, res: express.Response): void => { 
-    res.send('hello world' + req.params.n);                
-}
+    var value: any = req.params.n;
+    try {
+        if (NumberUtils.is32BitIntegerGreaterThanOrEqualTo1(value) === false) throw new RangeError('input must be within 1 to ' + NumberUtils.int32BitMax + ' range');
+
+        var primes: Array<number> = NumberUtils.generateArrayOfPrimes(value);
+        var hashset: Array<Array<number>> = NumberUtils.generateMultiplicationHashSet(primes);
+        var table = FormatUtils.formatHashsetToTable(hashset);
+
+        res.send(table);
+    }
+    catch (e) {
+        if (e instanceof TypeError) res.send('input [' + value + '] type invalid: ' + e.message);
+        else if (e instanceof RangeError) res.send(e.message);
+        else res.send('an unexpected error occurred with input [' + value + ']: ' + e.message);        
+    }            
+}               
