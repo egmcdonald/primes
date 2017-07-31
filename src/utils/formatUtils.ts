353cf6ebfe0579f7d7a7d5b8fe43046bd23cb925
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-31 23:40:28 +0100

    Updated format utility to format a table as an html snippet

--- src/utils/formatUtils.ts
+++ src/utils/formatUtils.ts
@@ -2,13 +2,13 @@ import * as ArrayUtils from './arrayUtils';
 
 export const formatHashsetToTable = (source: Array<Array<number>>): string => {
     if (source.length == 0) throw new RangeError('source array cannot be empty');
-    if (ArrayUtils.isNxN(source) === false) throw new RangeError('source array is not n x n');       
+    if (ArrayUtils.isNxN(source) === false) throw new RangeError('source array is not n x n');
 
-    var table: string = '';
-    for(var i = 0; i < source.length; i++) {
-        table += '|\t';
-        table += source[i].join('\t|\t');    
-        table += '\t|\r\n';
-    }
+    var table: string = '<table>';
+    for(var i = 0; i < source.length; i++)
+        table += formatTableRow(source[i]);
+    table += '</table>';
     return table;
 }
+
+const formatTableRow = (source: Array<any>): string => '<tr><td>' + source.join('</td><td>') + '</td></tr>';
