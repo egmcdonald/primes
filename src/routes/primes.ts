e6aa775e528cbbfd347da7eac681c93b50eb6d92
Emily McDonald <mcdonald.emily@live.co.uk>
2017-07-31 11:15:25 +0100

    Updated incorrect test for primes route - the previously configured expected table was not a multiplication table of primes

--- src/routes/primes.ts
+++ src/routes/primes.ts
@@ -1,13 +1,13 @@
 import * as express from 'express';
 
-import * as NumberUtils from './../NumberUtils';
-import * as FormatUtils from './../FormatUtils';
+import * as FormatUtils from './../formatUtils';
+import * as NumberUtils from './../numberUtils';
 
 export const route = (req: express.Request, res: express.Response): void => { 
     var value: any = req.params.n;
     try {
         if (NumberUtils.is32BitIntegerGreaterThanOrEqualTo1(value) === false) throw new RangeError('input must be within 1 to ' + NumberUtils.int32BitMax + ' range');
-
+        
         var primes: Array<number> = NumberUtils.generateArrayOfPrimes(value);
         var hashset: Array<Array<number>> = NumberUtils.generateMultiplicationHashSet(primes);
         var table = FormatUtils.formatHashsetToTable(hashset);
--- tests/routes/primes.spec.js
+++ tests/routes/primes.spec.js
@@ -15,8 +15,11 @@ describe ('routes/Primes', () => {
         it('should return multiplication table of primes up to param n', () => {
             var req = { params: { 'n': 3 } };
             var res = app.makeResponse((err, sideEffects) => {
-                var source = [[0, 1, 2, 3], [1, 1, 2, 3], [2, 2, 4, 6], [3, 3, 6, 9]];
-                assert.equal(sideEffects.send, FormatUtils.formatHashsetToTable(source));              
+                const expected = '|\t0\t|\t2\t|\t3\t|\t5\t|\r\n' +
+                                 '|\t2\t|\t4\t|\t6\t|\t10\t|\r\n' +
+                                 '|\t3\t|\t6\t|\t9\t|\t15\t|\r\n' +
+                                 '|\t5\t|\t10\t|\t15\t|\t25\t|\r\n';                
+                assert.equal(sideEffects.send, expected);              
             });
             Primes.route(req, res);
         });
