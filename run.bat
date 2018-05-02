b987867c38b666f87378f43f9c51ed460792f39e
Emily McDonald <mcdonald.emily@live.co.uk>
2018-05-02 13:44:42 +0100

    :fire: Removed unnecessary .bat files

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM open default browser with localhost
-echo opening default browser...
-start http://localhost:4141
-
-REM run app in node
-echo starting application in node...
-node scripts/app
